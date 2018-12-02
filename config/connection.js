eb9ffa4a210b605193c3173f7132f3eb5c25e623
David <40313600+dlara319@users.noreply.github.com>
2018-12-01 23:49:15 -0800

    final changes

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+var mysql = require("mysql");
+
+var source = {
+    localhost : {
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burger_db"
+    },
+
+    jawsDB: {
+        port: 3306,
+        host: "enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        user: "fs03oh0xws2vxv4b",
+        password: "wlmw0g6em7b491qv",
+        database: "xjtkg9d2p7hqvyom",
+    }
+}
+
+var connection = mysql.createConnection (source.jawsDB);
+
+connection.connect(function (err) {
+    if (err) {
+        console.error ("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log ("Database: " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+var express = require ("express");
+var router = express.Router();
+var burger = require("../models/burger.js");
+
+router.get ("/", function (req, res) {
+    res.redirect ("/burgers");
+});
+
+router.get ("/burgers", function (req, res) {
+    burger.selectAll(function (data) {
+        var burgersObject = { burgers: data };
+        res.render ("index", burgersObject);
+    });
+});
+
+router.post("/burgers/add", function (req, res) {
+    burger.insertOne ("burger_name", req.body.burger_name, function () {
+        res.redirect ("/burgers");
+    });
+});
+
+router.put ("/burgers/devour/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({ devoured: req.body.devoured }, condition, function () {
+        res.redirect("/burgers");
+    });
+});
+
+module.exports = router;
